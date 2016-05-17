fff145514c23999de78c7fd76208ffe0f899b5b1
Walter Chavarria <wchavarria03@gmail.com>
2016-05-17 00:24:33 -0600

    Updated some coding styles - code refactor

--- public/app/login/login.controller.js
+++ public/app/login/login.controller.js
@@ -10,17 +10,17 @@
         var vm = this;
 
         vm.login = function login(user,password) {
-            vm.error=false;
+            vm.error = false;
             var credentials = {
                 username: user,
                 password: password
             };
             LoginService.login(credentials);
             var user = JSON.parse(localStorage.getItem("loggedUser"));
-            if(!user){
-                vm.error=true;
-            }else{
-                vm.user={};
+            if(!user) {
+                vm.error = true;
+            } else {
+                vm.user = {};
                 $state.go('dashboard');
               //go to X view
             }
