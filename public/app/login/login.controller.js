0a6b0c55db126a57733e3e64768689ed393c6095
Walter Chavarria <wchavarria03@gmail.com>
2016-05-17 00:18:37 -0600

    Updated some coding styles - code refactor

--- public/app/login/login.controller.js
+++ public/app/login/login.controller.js
@@ -4,25 +4,25 @@
     var controllerId = 'LoginController';
     angular
         .module('app')
-        .controller(controllerId, [ '$scope','LoginService', '$state', LoginController ]);
+        .controller(controllerId, ['LoginService', '$state', LoginController ]);
 
-    function LoginController($scope,LoginService,$state) {
+    function LoginController(LoginService,$state) {
         var vm = this;
 
         vm.login = function login(user,password) {
-            vm.error=false;
+            vm.error = false;
             var credentials = {
                 username: user,
                 password: password
             };
             LoginService.login(credentials);
             var user = JSON.parse(localStorage.getItem("loggedUser"));
-            if(!user){
-                vm.error=true;
-            }else{
-                vm.user={};
+            if(!user) {
+                vm.error = true;
+            } else {
+                vm.user = {};
                 $state.go('dashboard');
-              //go to X view
+                //go to X view
             }
           };
       }
