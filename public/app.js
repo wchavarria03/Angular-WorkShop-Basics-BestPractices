87ff5dfd91d9748b0702c02324113e2799d562a5
Daniel Berrocal <jdanielb1992@gmail.com>
2016-05-17 20:01:47 -0600

    custom filter

--- public/app.js
+++ public/app.js
@@ -52,29 +52,39 @@
     function CustomFilter() {
         return function(items, search) {
             var out = [];
-
-            /*if (angular.isArray(items) && search !== "") {
-              items.forEach(function(item) {
-                var itemMatches = false;
-
-                var keys = Object.keys(props);
-                for (var i = 0; i < keys.length; i++) {
-                  var prop = keys[i];
-                  var text = props[prop].toLowerCase();
-                  if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
-                    itemMatches = true;
-                    break;
-                  }
-                }
-                if (itemMatches) {
-                  out.push(item);
-                }
-              });
-            } else {
-              // Let the output be the input untouched
-              out = items;
-          }*/
-
+            var model = ['id','name','email','body'];
+            if (angular.isArray(items) && search !== ""){
+                var splitSearch = search.toLowerCase().split(' ');
+                splitSearch.forEach(function splitSearchForEach(searchText) {
+                    if (out.length) {
+                        items = out;
+                    }
+                    items.forEach(function itemsForEach(item){
+                        var itemMatches = false;
+                        for (var i = 0; i < model.length; i++) {
+                            var prop = model[i];
+                            if(item[prop] && item[prop].toString().toLowerCase().indexOf(searchText) !== -1 ){
+                                itemMatches = true;
+                                break;
+                            }
+                        }
+                        if (itemMatches) {
+                            var existItem = out.some(function checkItemPushed(outItem) {
+                                return outItem.id == item.id;
+                            });
+                            if (!existItem) {
+                                out.push(item);
+                            }
+                        } else {
+                            out = out.filter(function (e) {
+                                return e.id !== item.id ? item : null;
+                            })
+                        }
+                    });
+                });
+            }else{
+                out = items;
+            }
             return out;
       };
     }
